d65085d08e22f41d7fc7c415de41e88a3a363d97
Berlin <Berline_lucien@yahoo.com>
2022-06-10 00:16:15 -0400

    exercise 1 - 4 complete

--- DataTypes_Variables_Exercise.js
+++ DataTypes_Variables_Exercise.js
@@ -12,13 +12,17 @@
 // Data Types and Variables
 
 // Exercise 1. Simply declare a variable named 'emptyVariable'
+let emptyVariable;
 
 // Exercise 2. Declare a variable called 'myName'
+//let myName;
 
 // Exercise 3. Assign your name as the value for 'myName'
-
+let myName = 'Berline';
+console.log(myName);
 // Exercise 4. Declare a variable called dreamDestination and give it the value
 // of what your dream vacation destination is.
+let dreamDestination = 'Budapest';
 
 // Exercise 5. Make the following variables and assignments
 // - a variable called num1 with a value greater than 1 and less than 10
