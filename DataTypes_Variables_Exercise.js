322f4d1c6634381deb4de7d380cb599fe56312cb
Berlin <Berline_lucien@yahoo.com>
2022-06-10 00:27:08 -0400

    completed exercise 4-10

--- DataTypes_Variables_Exercise.js
+++ DataTypes_Variables_Exercise.js
@@ -15,39 +15,58 @@
 let emptyVariable;
 
 // Exercise 2. Declare a variable called 'myName'
-//let myName;
+let myName;
 
 // Exercise 3. Assign your name as the value for 'myName'
 let myName = 'Berline';
-console.log(myName);
+//console.log(myName);
+
 // Exercise 4. Declare a variable called dreamDestination and give it the value
 // of what your dream vacation destination is.
 let dreamDestination = 'Budapest';
+//console.log(dreamDestination);
 
 // Exercise 5. Make the following variables and assignments
 // - a variable called num1 with a value greater than 1 and less than 10
 // - a variable called num2 with a value of greater than or equal to 1 and less
 // than or equal to 10
+let num1 = [2, 3, 4, 5, 6, 7, 8, 9];
+let num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Exercise 6. Write a comment below explaining the difference between the rules
 // for num1 and num2 from Exercise 5. What numbers would be valid values for 
 // num2 but not num1?
-//
+// num2 includes 1 and 10, since the statements say " greater than or equal"
+// num1 does not include 1 and 10, because it does not have "equal to"
+
 
 // Exercise 7. Now we will try some addition.
 // a. Assign the the values of 4 and 6 to num1 and num2, respectively.
 // b. Make a new variable called 'theSum', and use 'num1' and
 // 'num2' to assign its value using the "+" operator.
+let num1 = 4;
+let num2 = 6;
+let theSum = num1 + num2;
+console.log(theSum);
+// output theSum = 10
 
 // Exercise 8. Now we will try some multiplication.
 // Make a new variable called 'theProduct', multiply num1 and num2 and assign
 // the resulting value to 'theProduct'.
+let theProduct = num1 * num2
+console.log(theProduct);
+// output theProduct = 24
 
 // Exercise 9.
 // Make a new variable called 'statement' and using 'myName' and
 // 'dreamDestination' and the concatenation method of your choice, make the
 // value of statement to be:
 // "Hi, my name is <your name>, and I can't wait to visit <your destination>!"
+let statement = `Hi, my name is ${myName}, and I can't wait to visit ${dreamDestination}!`
+console.log(statement); 
+// output Hi, my name is Berlin, and I can't wait to visit Budapest!
+
+
 
 // *NOTE* For the following exercises, put your answers in the line below
 // the description of each exercise. For example, below a prompt, you may see:
