1785ce73c54d841aeb6a282771590407feda18ec
Berlin <Berline_lucien@yahoo.com>
2022-07-04 22:54:43 -0400

    completed task 4

--- Week4/JS fundamentals:Objects.js
+++ Week4/JS fundamentals:Objects.js
@@ -23,9 +23,9 @@ console.log(user);
 
 console.log("***** TASK 2 *******");
 /** Check for emptiness
-* Write the function isEmpty(obj) which return s true if the
-* object has no properties, false otherwise
-*/
+ * Write the function isEmpty(obj) which return s true if the
+ * object has no properties, false otherwise
+ */
 let schedule = {};
 //alert( isEmpty(schedule) ); // true
 schedule["8:30"] = "get up";
@@ -40,20 +40,38 @@ function isEmpty(obj) {
 console.log(isEmpty());
 console.log(isEmpty(schedule));
 
-
 console.log("***** TASK 3 *******");
 /** Sum object properties
  * Write the code to sum all salaries and store in the variable sum
  * Should be 390. If salaries is empty, then the result must be 0
  */
 let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-}
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
 // console.log(salaries);
 let sum = 0;
-for (let key in salaries){
-    sum += salaries[key];
+for (let key in salaries) {
+  sum += salaries[key];
 }
 console.log(sum);
+
+console.log("***** TASK 4 *******");
+/** Multiply numeric property values by 2
+ * Create a function multiplyNumeric(obj) that multiplies all numeric
+ * property values of obj by (2)
+ */
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+function multiplyNumeric(menuItem){
+for (let key in menuItem){
+    if (typeof menuItem[key] == 'number'){
+        menuItem[key] *= 2;
+    } return menuItem   
+}
+}
+console.log(multiplyNumeric(menu));
